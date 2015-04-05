c75347acaf0112bdba64cba840d86265078d0fe6
Timothée Peignier <timothee.peignier@tryphon.org>
2015-04-05 13:43:50 -0700

    Move things around.

--- lib/sequel_password.rb
+++ lib/sequel_password.rb
@@ -3,6 +3,7 @@ require "bcrypt"
 require "openssl"
 require "pbkdf2"
 require "securerandom"
+require "sequel_password/hashers"
 
 module Sequel
   module Plugins
@@ -33,10 +34,6 @@ module Sequel
           hasher(algorithm).encode(password, salt)
         end
 
-        def hasher(algorithm = :default)
-          @hashers.fetch(algorithm.to_sym, @hashers.values.first)
-        end
-
         def usable_password?(encoded)
           return false if encoded.nil? || encoded.start_with?("!")
 
@@ -58,6 +55,12 @@ module Sequel
 
           correct
         end
+
+        private
+
+        def hasher(algorithm = :default)
+          @hashers.fetch(algorithm.to_sym, @hashers.values.first)
+        end
       end
 
       module InstanceMethods
@@ -72,113 +75,6 @@ module Sequel
 
         def set_unusable_password
           send("#{model.column}=", model.make_password(nil))
-        end
-      end
-
-      class Hasher
-        attr_reader :algorithm
-
-        def salt
-          # 72 bits
-          SecureRandom.hex(9)
-        end
-
-        def verify(password, encoded)
-          raise NotImplementedError
-        end
-
-        def encode(password, salt)
-          raise NotImplementedError
-        end
-
-        def must_update(encoded)
-          false
-        end
-
-        private
-
-        def constant_time_compare(a, b)
-          check = a.bytesize ^ b.bytesize
-          a.bytes.zip(b.bytes) { |x, y| check |= x ^ y }
-          check == 0
-        end
-      end
-
-      class PBKDF2Hasher < Hasher
-        def initialize
-          @algorithm = :pbkdf2_sha256
-          @iterations = 24000
-          @digest = OpenSSL::Digest::SHA256.new
-        end
-
-        def encode(password, salt, iterations = nil)
-          iterations = @iterations if iterations.nil?
-          hash = PBKDF2.new(password: password, salt: salt,
-            iterations: iterations, hash_function: @digest)
-          hash = Base64.strict_encode64(hash.value)
-          "#{@algorithm}$#{iterations}$#{salt}$#{hash}"
-        end
-
-        def verify(password, encoded)
-          algorithm, iterations, salt, hash = encoded.split('$', 4)
-          hash = encode(password, salt, iterations.to_i)
-          constant_time_compare(encoded, hash)
-        end
-
-        def must_update(encoded)
-          algorithm, iterations, salt, hash = encoded.split('$', 4)
-          iterations.to_i != @iterations
-        end
-      end
-
-      class BCryptSHA256Hasher < Hasher
-        def initialize
-          @algorithm = :bcrypt_sha256
-          @cost = 12
-          @digest = OpenSSL::Digest::SHA256.new
-        end
-
-        def salt
-          BCrypt::Engine.generate_salt(@cost)
-        end
-
-        def encode(password, salt)
-          password = @digest.digest(password) unless @digest.nil?
-          hash = BCrypt::Engine.hash_secret(password, salt)
-          "#{@algorithm}$#{hash}"
-        end
-
-        def verify(password, encoded)
-          algorithm, data = encoded.split('$', 2)
-          password = @digest.digest(password) unless @digest.nil?
-          hash = BCrypt::Engine.hash_secret(password, data)
-          constant_time_compare(data, hash)
-        end
-      end
-
-      class BCryptHasher < BCryptSHA256Hasher
-        def initialize
-          @algorithm = :bcrypt
-          @cost = 12
-          @digest = nil
-        end
-      end
-
-      class SHA1Hasher < Hasher
-        def initialize
-          @algorithm = :sha1
-          @digest = OpenSSL::Digest::SHA1.new
-        end
-
-        def encode(password, salt)
-          hash = @digest.digest(salt + password).unpack('H*').first
-          "#{@algorithm}$#{salt}$#{hash}"
-        end
-
-        def verify(password, encoded)
-          algorithm, salt, hash = encoded.split('$', 3)
-          hash = encode(password, salt)
-          constant_time_compare(encoded, hash)
         end
       end
     end
---
+++ lib/sequel_password/hashers.rb
@@ -0,0 +1,112 @@
+module Sequel
+  module Plugins
+    module Password
+      class Hasher
+        attr_reader :algorithm
+
+        def salt
+          # 72 bits
+          SecureRandom.hex(9)
+        end
+
+        def verify(password, encoded)
+          raise NotImplementedError
+        end
+
+        def encode(password, salt)
+          raise NotImplementedError
+        end
+
+        def must_update(encoded)
+          false
+        end
+
+        private
+
+        def constant_time_compare(a, b)
+          check = a.bytesize ^ b.bytesize
+          a.bytes.zip(b.bytes) { |x, y| check |= x ^ y }
+          check == 0
+        end
+      end
+
+      class PBKDF2Hasher < Hasher
+        def initialize
+          @algorithm = :pbkdf2_sha256
+          @iterations = 24000
+          @digest = OpenSSL::Digest::SHA256.new
+        end
+
+        def encode(password, salt, iterations = nil)
+          iterations = @iterations if iterations.nil?
+          hash = PBKDF2.new(password: password, salt: salt,
+            iterations: iterations, hash_function: @digest)
+          hash = Base64.strict_encode64(hash.value)
+          "#{@algorithm}$#{iterations}$#{salt}$#{hash}"
+        end
+
+        def verify(password, encoded)
+          algorithm, iterations, salt, hash = encoded.split('$', 4)
+          hash = encode(password, salt, iterations.to_i)
+          constant_time_compare(encoded, hash)
+        end
+
+        def must_update(encoded)
+          algorithm, iterations, salt, hash = encoded.split('$', 4)
+          iterations.to_i != @iterations
+        end
+      end
+
+      class BCryptSHA256Hasher < Hasher
+        def initialize
+          @algorithm = :bcrypt_sha256
+          @cost = 12
+          @digest = OpenSSL::Digest::SHA256.new
+        end
+
+        def salt
+          BCrypt::Engine.generate_salt(@cost)
+        end
+
+        def encode(password, salt)
+          password = @digest.digest(password) unless @digest.nil?
+          hash = BCrypt::Engine.hash_secret(password, salt)
+          "#{@algorithm}$#{hash}"
+        end
+
+        def verify(password, encoded)
+          algorithm, data = encoded.split('$', 2)
+          password = @digest.digest(password) unless @digest.nil?
+          hash = BCrypt::Engine.hash_secret(password, data)
+          constant_time_compare(data, hash)
+        end
+      end
+
+      class BCryptHasher < BCryptSHA256Hasher
+        def initialize
+          @algorithm = :bcrypt
+          @cost = 12
+          @digest = nil
+        end
+      end
+
+      class SHA1Hasher < Hasher
+        def initialize
+          @algorithm = :sha1
+          @digest = OpenSSL::Digest::SHA1.new
+        end
+
+        def encode(password, salt)
+          hash = @digest.digest(salt + password).unpack('H*').first
+          "#{@algorithm}$#{salt}$#{hash}"
+        end
+
+        def verify(password, encoded)
+          algorithm, salt, hash = encoded.split('$', 3)
+          hash = encode(password, salt)
+          constant_time_compare(encoded, hash)
+        end
+      end
+    end
+  end
+end
